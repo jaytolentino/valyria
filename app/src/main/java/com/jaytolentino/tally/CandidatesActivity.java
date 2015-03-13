6eccf7bab4aca68cf99e605357a0c54f9a619dea
Jay Tolentino <jay@nuzzel.com>
2015-03-12 22:14:34 -0700

    initial commit

---
+++ app/src/main/java/com/jaytolentino/tally/CandidatesActivity.java
@@ -0,0 +1,34 @@
+package com.jaytolentino.tally;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class CandidatesActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidates);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_candidates, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
